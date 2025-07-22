28ea29f8fea22fba63adbf987ffb35d860c2843f
Manasa66316 <manasach66316@gmail.com>
2025-07-22 09:13:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 10000;
 
 // ✅ Serve static files (CSS, JS, images, etc.)
-app.use(express.static(path.join(__dirname,'style.css')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
